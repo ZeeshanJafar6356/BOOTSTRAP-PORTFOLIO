d98bc912e2d2f127eb9f3661eca2ae2dd0828cbb
Zeeshan <Zeeshanjafar6356@gmail.com>
2022-09-07 16:46:13 +0500

    first commit

---
+++ BOOTSTRAP/index.js
@@ -0,0 +1,9 @@
+document.getElementById('sub');
+function Test() {
+  window.alert('Your Query Has been Submitted!');
+}
+
+const btn = document.getElementById('test');
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
